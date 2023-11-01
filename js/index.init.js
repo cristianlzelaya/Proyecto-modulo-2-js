179df7e455f76d9925eddfe7a5f38f2dd1ec9fc1
Bruno Tornese <brunoluchi2003@gmail.com>
2023-10-31 21:10:23 -0300

    Preparado del html y css del navbar, se deja listo para armar funciones del js para los botones del nav

---
+++ js/index.init.js
@@ -0,0 +1,12 @@
+// carga del navbar y botones
+document.addEventListener("DOMContentLoaded", function () {
+  const inicioButton = document.querySelector("#inicioButton");
+  const registroButton = document.querySelector("#registroButton");
+  const loginButton = document.querySelector("#loginButton");
+
+  inicioButton.addEventListener("click", function () {});
+
+  registroButton.addEventListener("click", function () {});
+
+  loginButton.addEventListener("click", function () {});
+});
