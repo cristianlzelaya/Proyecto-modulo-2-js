197ceb6cc02d47a4b7247858fffa568eac84f09f
Bruno Tornese <brunoluchi2003@gmail.com>
2023-11-08 17:14:20 -0300

    acomodado de los imports e exports del index y redirecciones a vistas

--- js/index.init.js
+++ js/index.init.js
@@ -7,9 +7,6 @@ let movies;
 document.addEventListener("DOMContentLoaded", function () {
   Navbar();
   inicioButton.addEventListener("click", function (e) {
-    registroButton.addEventListener("click", function () {});
-    loginButton.addEventListener("click", function () {});
-    console.log("inicio");
     setMovies();
     movies = getMovies();
     console.log(movies);
