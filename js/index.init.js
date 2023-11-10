b185f040e2414ae2ed0f831ae8f23c58c7e11a03
Lucia Craven Garat <luciacraven93@gmail.com>
2023-11-10 14:33:03 -0300

    actualizo carrusel

--- js/index.init.js
+++ js/index.init.js
@@ -2,31 +2,28 @@ import { redireccionarInicio } from "./utils/redireccionarInicio.js";
 import Navbar from "../components/navbar.js";
 import { setMovies } from "./services/setMovies.js";
 import { getMovies } from "./services/getMovies.js";
-import estrenosCategorie from "../components/estrenosCategorie.js";
+import {createCarruselItem} from "./carrusel.js";
+import {getEstrenos} from "./services/getEstrenos.js"
+
 let movies;
-
-let pelis = [];
-
 document.addEventListener("DOMContentLoaded", function () {
   Navbar();
-  // inicioButton.addEventListener("click", function (e) {
-  //   redireccionarInicio();
-  //   registroButton.addEventListener("click", function () {});
-  // loginButton.addEventListener("click", function () {});
-  // console.log("inicio");
+  //redireccionarInicio();
   setMovies();
   movies = getMovies();
-  console.log(movies);
-  renderEstrenosCategorie(movies);
-  // });
+  getEstrenos()
+  createCarruselItem()
 });
+
+console.log(getMovies('movies'));
+
 
 const inicioButton = document.getElementById("inicioButton");
 const registroButton = document.getElementById("registroButton");
 const loginButton = document.getElementById("loginButton");
 
 //------------Categorías-------------//
-// const estrenosContainer = document.getElementById("estrenosContainer");
+// 
 
 // const renderEstrenosCategorie = (pelis) => {
 //   estrenosContainer.innerHTML = "";
@@ -40,6 +37,12 @@ const loginButton = document.getElementById("loginButton");
 //     }
 //   });
 // };
+
+
+//const estrenosContainer = document.getElementsByClassName('glider');
+
+
+
 
 
 
--- js/services/setMovies.js
+++ js/services/setMovies.js
@@ -1,10 +1,10 @@
 import moviesDb from "../../database/peliculas/series.json" assert { type: "json" };
 
 export const setMovies = () => {
-    console.log("getitem");
+    //console.log("getitem");
   const movies = JSON.parse(localStorage.getItem("movies"));
   if (!movies) {
-    console.log("hola");
+    //console.log("hola");
     localStorage.setItem("movies", JSON.stringify(moviesDb));
   }
 };
