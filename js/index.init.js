082821747e3514d07bf93c6a38379a488df57700
Lucia Craven Garat <luciacraven93@gmail.com>
2023-11-10 21:15:09 -0300

    actualizo carruseles 2

--- js/index.init.js
+++ js/index.init.js
@@ -2,44 +2,31 @@ import { redireccionarInicio } from "./utils/redireccionarInicio.js";
 import Navbar from "../components/navbar.js";
 import { setMovies } from "./services/setMovies.js";
 import { getMovies } from "./services/getMovies.js";
-import {createCarruselItem} from "./carrusel.js";
-import {getEstrenos} from "./services/getEstrenos.js"
+import {getEstrenos} from "./services/getEstrenos.js";
+import { getPeliculas } from "./services/getPeliculas.js";
+import {getSeries} from "./services/getSeries.js";
+import { getDocumentales } from "./services/getDocumentales.js";
 
 let movies;
 document.addEventListener("DOMContentLoaded", function () {
   Navbar();
-  //redireccionarInicio();
   setMovies();
   movies = getMovies();
-  getEstrenos()
-  createCarruselItem()
+  getEstrenos();
+  getPeliculas();
+  getSeries();
+  getDocumentales();
 });
-
-console.log(getMovies('movies'));
 
 
 const inicioButton = document.getElementById("inicioButton");
 const registroButton = document.getElementById("registroButton");
 const loginButton = document.getElementById("loginButton");
 
-//------------Categorías-------------//
-// 
-
-// const renderEstrenosCategorie = (pelis) => {
-//   estrenosContainer.innerHTML = "";
-  
-//   pelis.map((movie) => {
-//     const estreno = movie.categoria === "estreno";
-    
-//     if (estreno) {
-//       console.log(movie);
-//       estrenosContainer.innerHTML += estrenosCategorie(movie);
-//     }
-//   });
-// };
 
 
-//const estrenosContainer = document.getElementsByClassName('glider');
+
+
 
 
 
