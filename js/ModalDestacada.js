4e2242947c351bb048cd7d7f1acdd997afbaba63
Bruno Tornese <brunoluchi2003@gmail.com>
2023-11-12 22:55:33 -0300

    Terminado de iframe separado para el trailer peliculas

--- js/ModalDestacada.js
+++ js/ModalDestacada.js
@@ -1,27 +1,72 @@
+const abrirVentana = (pelicula) => {
+  var titulo = pelicula.nombre;
+  var ventanaContenido = "<h2>" + titulo + "</h2>";
+  if (pelicula.video) {
+    let videoId = pelicula.video.split("v=")[1].split("&")[0];
+    let embedUrl = "https://www.youtube.com/embed/" + videoId;
+    ventanaContenido +=
+      '<iframe width="560" height="315" src="' +
+      embedUrl +
+      '" frameborder="0" allowfullscreen></iframe>';
+  } else {
+    ventanaContenido += "<p>Video no disponible</p>";
+  }
+  let nuevaVentana = window.open("", "_blank");
+  nuevaVentana.document.write(ventanaContenido);
+};
+
 const mostrarModal = (pelicula) => {
   document.getElementById("modalNombre").textContent = pelicula.nombre;
   document.getElementById("modalCategoria").textContent = pelicula.categoria;
   document.getElementById("modalDescripcion").textContent =
     pelicula.descripcion;
   document.getElementById("modalImagen").src = pelicula.imagen;
-  if (pelicula.imagenDestacada) {
-    document.getElementById("modalImagenDestacada").src =
-      pelicula.imagenDestacada;
-    document.getElementById("modalImagenDestacada").style.display = "block";
-  } else {
-    document.getElementById("modalImagenDestacada").style.display = "none";
-  }
+  document.getElementById("modalImagenDestacada").src =
+    pelicula.imagenDestacada;
+  const verTrailerBtn = document.getElementById("verTrailerBtn");
   if (pelicula.video) {
     let videoId = pelicula.video.split("v=")[1].split("&")[0];
     let embedUrl = "https://www.youtube.com/embed/" + videoId;
-
-    let iframe = document.getElementById("modalVideoIframe");
-    iframe.src = embedUrl;
-    iframe.style.display = "block";
+    verTrailerBtn.style.display = "block";
+    verTrailerBtn.addEventListener("click", function () {
+      const nuevaVentana = window.open("", "_blank");
+      nuevaVentana.document.write(`
+            <html>
+            <head>
+                <title>${pelicula.nombre}</title>
+                <style>
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        overflow: hidden;
+                        background-color: black; /* Fondo negro */
+                    }
+                    .video-container {
+                        width: 100%;
+                        height: 100vh;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    iframe {
+                        width: 90%;
+                        height: 90%;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="video-container">
+                    <iframe src="${embedUrl}" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </body>
+            </html>
+        `);
+    });
   } else {
-    document.getElementById("modalVideoIframe").style.display = "none";
+    // Si no hay video oculta el botón
+    verTrailerBtn.style.display = "none";
   }
   $("#detallesModal").modal("show");
 };
 
-export { mostrarModal };
+export { mostrarModal, abrirVentana };
